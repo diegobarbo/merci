c9b139962429ccc6fcda4463a7931d61494ab71a
Diego Barbo <diegobarbox@gmail.com>
2022-05-07 11:05:38 -0300

    user pages based on permissions

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import Peca
 
 
+@login_required
 def index(request):
     return render(request, 'dashboard/index.html')
 
 
+@login_required
 def staff(request):
     return render(request, 'dashboard/staff.html')
 
 
+@login_required
 def pecas(request):
     return render(request, 'dashboard/pecas.html')
 
+
+@login_required
 def vendas(request):
     return render(request, 'dashboard/vendas.html')
