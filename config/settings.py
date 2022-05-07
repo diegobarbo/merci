79987dd4e19da0b0d0f2917d764991a1cfcb5168
Diego Barbo <diegobarbox@gmail.com>
2022-05-07 12:23:49 -0300

    profile page and image file

--- config/settings.py
+++ config/settings.py
@@ -122,6 +122,11 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = (BASE_DIR / "asert")
 
+MEDIA_ROOT = (BASE_DIR / 'media')
+
+MEDIA_URL = '/media/'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
